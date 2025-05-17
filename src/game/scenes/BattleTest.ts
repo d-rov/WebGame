522bff6ef36473d6c09a83d52a54a9f85993810a
duncan-ro <duncan.rov@gmail.com>
2025-05-16 23:44:56 -0600

    Adding a BattleTest scene and updated MainMenu

---
+++ src/game/scenes/BattleTest.ts
@@ -0,0 +1,11 @@
+import { Scene } from "phaser";
+
+export class BattleTest extends Scene {
+  constructor() {
+    super("BattleTest");
+  }
+
+  create() {
+    console.log("Create function in BattleTest");
+  }
+}
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -1,32 +1,69 @@
-import { Scene, GameObjects } from 'phaser';
+import { Scene, GameObjects } from "phaser";
 
-export class MainMenu extends Scene
-{
-    background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
+export class MainMenu extends Scene {
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  title: GameObjects.Text;
+  playButton: GameObjects.Text;
+  testButton: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
-    }
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+  create() {
+    this.background = this.add.image(512, 384, "background");
 
-        this.logo = this.add.image(512, 300, 'logo');
+    this.logo = this.add.image(512, 150, "logo");
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.title = this.add
+      .text(512, 250, "Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+    this.playButton = this.add
+      .text(512, 350, "Play", {
+        fontFamily: "Arial Black",
+        fontSize: 44,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-            this.scene.start('Game');
+    this.testButton = this.add
+      .text(512, 450, "Battle Test", {
+        fontFamily: "Arial Black",
+        fontSize: 44,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        });
-    }
+    const testButton = this.testButton.setInteractive(
+      new Phaser.Geom.Rectangle(0, 0, 275, 40),
+      Phaser.Geom.Rectangle.Contains
+    );
+
+    testButton.on("pointerover", () => {
+      testButton.setTint(0x7878ff);
+    });
+
+    testButton.on("pointerout", () => {
+      testButton.clearTint();
+    });
+
+    testButton.on("pointerdown", () => {
+      this.scene.start("BattleTest");
+    });
+  }
 }
